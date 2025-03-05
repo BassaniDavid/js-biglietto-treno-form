b3892fe3c82251ebf9d361a7ecb2233acf74b4ba
BassaniDavid <davbassan98@gmail.com>
2025-03-05 16:16:31 +0100

    feat: milestone2 complete

--- js/app.js
+++ js/app.js
@@ -1,55 +1,79 @@
+// kilometri
+const kmIns = document.getElementById('km')
 
-const kmIns= document.getElementById('km')
-console.log(kmIns)
+// età
+const ageIns = document.getElementById('age')
 
-const ageIns= document.getElementById('age')
-console.log(ageIns)
-
+// costo al km
 const costKm = 0.21
 
-const under18Discount= 20
+// sconto minorenni
+const under18Discount = 20
 
-const over65Discount= 40
+// sconto over65
+const over65Discount = 40
 
-const submit= document.getElementById('submit')
-console.log(submit)
+// bottone per inviare i dati
+const submit = document.getElementById('submit')
 
-const form= document.querySelector('form')
-console.log(form)
+// form
+const form = document.querySelector('form')
 
-form.addEventListener('submit', (event) =>{
+// div con paragrafo con risultato
+const divResult = document.querySelector('div')
+
+// paragrafo per risultato
+const result = document.getElementById('recap')
+
+// previene che la pagina si ricarichi e che i dati vengano persi
+form.addEventListener('submit', (event) => {
     event.preventDefault()
 })
 
-submit.addEventListener('click', () =>{
+// invia i dati al click del bottone
+submit.addEventListener('click', () => {
+
     // prendo il valore di km inserito
-    const km= parseInt(kmIns.value)
+    const km = parseInt(kmIns.value)
     console.log(km)
 
     // prendo l'età inserita
-    const age=parseInt(ageIns.value)
+    const age = parseInt(ageIns.value)
     console.log(age)
 
     // controllo che siano effettivamente numeri
-    if (isNaN(km)){
-        return("per favore, inserisci un numero di km valido")
+    if (isNaN(km)) {
+        return ("per favore, inserisci un numero di km valido")
     };
-    if (isNaN(age)){
-        return("per favore, inserisci un età valida")
+    if (isNaN(age)) {
+        return ("per favore, inserisci un età valida")
     };
 
-    if (age < 18 ){
+    // inserisco i dati inseriti nel recap
+    const firstPar = document.createElement("p")
+    firstPar.innerText ='i numeri di kilometri inseriti sono:' + km
+    result.appendChild(firstPar)
+
+    const secondPar = document.createElement("p")
+    secondPar.innerText ="l'età inserita è: " + age
+    result.appendChild(secondPar)
+
+    // inserisco il risultato nel recap
+    if (age < 18) {
         let totalCost = (km * costKm) / 100 * (100 - under18Discount)
-        console.log(" il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+        result.append(" il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
     }
-    else if ( age > 65 ){
+    else if (age > 65) {
         let totalCost = (km * costKm) / 100 * (100 - over65Discount)
-        console.log(" il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+        result.append(" il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
     }
-    else{
+    else {
         let totalCost = (km * costKm)
-        console.log(" il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+        result.append(" il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
     };
+
+    // rendo visibile il div
+    divResult.classList.remove('invisible')
 
 })
 
